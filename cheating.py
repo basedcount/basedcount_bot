66e25f68af1d91b345110cde78efd9c23cf86692
Squidtoon99 <49101235+Squidtoon99@users.noreply.github.com>
2022-03-31 00:03:39 -0500

    chore(cheating): Easier and more legible code

--- cheating.py
+++ cheating.py
@@ -11,45 +11,36 @@ from pymongo import MongoClient
 
 # Connect to Reddit
 reddit = praw.Reddit(client_id=bot.client_id,
-			client_secret=bot.client_secret,
-			user_agent=bot.user_agent,
-			username=bot.username,
-			password=bot.password)
+                     client_secret=bot.client_secret,
+                     user_agent=bot.user_agent,
+                     username=bot.username,
+                     password=bot.password)
+
 
 def connectMongo_History():
-	cluster = MongoClient(mongoPass)
-	dataBased = cluster['dataBased']
-	return dataBased['basedHistory']
+    cluster = MongoClient(mongoPass)
+    dataBased = cluster['dataBased']
+    return dataBased['basedHistory']
 
 
 def checkForCheating(user, parentAuthor):
-	basedHistory = connectMongo_History()
+    basedHistory = connectMongo_History()
+    basedHistory.find_one_and_update({"name": user}, {"$inc": {parentAuthor: 1}}, upsert=True)
 
-	# Add users to database
-	userProfile = basedHistory.find_one({'name':user})
-	if userProfile == None:
-		basedHistory.update_one({'name': user}, {'$set': {parentAuthor: 1}}, upsert=True)
-	else:
-		if parentAuthor not in userProfile:
-			basedHistory.update_one({'name': user}, {'$set': {parentAuthor: 1}})
-		else:
-			basedHistory.update_one({'name': user}, {'$set': {parentAuthor: userProfile[parentAuthor] + 1}})
 
-	
 def sendCheatReport():
-	basedHistory = connectMongo_History()
-	userProfile = basedHistory.find({})
+    basedHistory = connectMongo_History()
+    userProfile = basedHistory.find({})
 
-	# Add Suspicious Users
-	content = ''
-	for user in userProfile:
-		for key in user:
-			if (key != '_id' and key != 'name'):
-				if user[key] > 5:
-					content = content + user['name'] + ' based ' + str(key) + ' ' + str(user[key]) + ' times.\n'
+    # Add Suspicious Users
+    content = ''
+    for user in userProfile:
+        for key in user:
+            if (key != '_id' and key != 'name' and user[key] > 5):
+                content += f"{user['name']} based {key} {user[key]} times.\n"
 
-	# Send Cheat Report to Admin
-	if content != '':
-		reddit.redditor(bot.admin).message('Cheat Report', content)
+    # Send Cheat Report to Admin
+    if content != '':
+        reddit.redditor(bot.admin).message('Cheat Report', content)
 
-	basedHistory.delete_many({})
+    basedHistory.delete_many({})
