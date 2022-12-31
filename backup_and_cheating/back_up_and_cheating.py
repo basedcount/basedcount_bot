91d748bca1e9950d69f62b19dd1a09f813ea7665
isFakeAccount <trevorphillips@gmx.us>
2022-12-31 02:18:31 -0500

    Added google drive backup method
    - Added the functions related to uploading databased to google drive
    - Updated requirements.txt

--- backup_and_cheating/back_up_and_cheating.py
+++ backup_and_cheating/back_up_and_cheating.py
@@ -4,43 +4,59 @@ from os import getenv
 
 import aioschedule as schedule
 from dotenv import load_dotenv
+
+from backup_drive import backup_databased
 from utility_functions import get_mongo_client, get_mongo_collection, send_message_to_admin
 
 load_dotenv("../.env")
 
 
 async def send_cheating_report() -> None:
+    """Sends the based history records to bot admin if a user has given another user more than 5 based
+
+    :returns: None
+
+    """
     async with get_mongo_client() as mongo_client:
         based_history_collection = await get_mongo_collection("basedHistory", mongo_client=mongo_client)
         based_history = await based_history_collection.find({}).to_list(length=None)
 
-        report_list = []
+        sorted_transactions: list[tuple[str, str, int]] = []
         for based_transaction in based_history:
             if based_transaction.get("count", 0) > 5:
-                report_list.append(f"- {based_transaction['from']} based {based_transaction['to']} {based_transaction['count']} times")
+                sorted_transactions.append((based_transaction["from"], based_transaction["to"], based_transaction["count"]))
 
-        report = "\n".join(report_list)
+        sorted_transactions.sort(key=lambda x: x[2], reverse=True)
+        report = "\n".join(f"- {x[0]} based {x[1]} {x[2]} times" for x in sorted_transactions)
         await send_message_to_admin("Cheating Report", report, getenv("REDDIT_USERNAME", "basedcount_bot"))
+        await based_history_collection.delete_many({})
 
 
-async def backup_databased() -> None:
-    return None
+async def backup() -> None:
+    """Backs up the entire databased on Google Drive
+
+    :returns: None
+
+    """
+    async with get_mongo_client() as mongo_client:
+        users_collection = await get_mongo_collection("users", mongo_client=mongo_client)
+        users = await users_collection.find({}).to_list(length=None)
+        await asyncio.to_thread(backup_databased, users)
 
 
 async def task_scheduler() -> None:
     cheating_report_task = asyncio.create_task(send_cheating_report())
-    backup_task = asyncio.create_task(backup_databased())
+    backup_task = asyncio.create_task(backup())
     await cheating_report_task
     await backup_task
 
 
 def main() -> None:
-    asyncio.run(send_cheating_report())
-    schedule.every(5).seconds.do(task_scheduler)
+    schedule.every(1).days.do(task_scheduler)
     while True:
         asyncio.run(schedule.run_pending())
         time.sleep(0.1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
