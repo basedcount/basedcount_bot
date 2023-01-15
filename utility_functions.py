ac133afc0bfe0d07035a1832b03fbca79fcbeedc
isFakeAccount <trevorphillips@gmx.us>
2023-01-15 14:35:16 -0500

    Added colored logging

--- utility_functions.py
+++ utility_functions.py
@@ -6,7 +6,10 @@ from os import getenv
 
 from asyncpraw import Reddit
 from asyncpraw.reddit import Redditor
+from colorlog import ColoredFormatter
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
+
+config.fileConfig("logging.conf")
 
 
 @asynccontextmanager
@@ -75,5 +78,10 @@ def create_logger(logger_name: str) -> Logger:
     :returns: Logging Object.
 
     """
-    config.fileConfig("logging.conf")
+
+    log_format = "%(log_color)s[%(asctime)s] %(levelname)s [%(filename)s.%(funcName)s:%(lineno)d] %(message)s"
+    logger = getLogger(logger_name)
+    for handler in logger.handlers:
+        handler.setFormatter(ColoredFormatter(log_format))
+
     return getLogger(logger_name)
