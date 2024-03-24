4615e0fa7d1a9815d4e8472ad391e98f413718a1
Yoshikage Kira <trevorphillips@gmx.us>
2024-03-24 04:00:31 -0400

    Adds shebang to .venv so I don't have to activate it manually

--- basedcount_bot.py
+++ basedcount_bot.py
@@ -1,3 +1,4 @@
+#!.venv/bin/python
 from __future__ import annotations
 
 import asyncio
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-source venv/bin/activate
-pm2 start basedcount_bot.py --name basedcount_bot
